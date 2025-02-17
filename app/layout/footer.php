2c0dc2ff66001a570fac2e9216b9265b36bfa5d9
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-17 11:16:55 +0530

    index page

---
+++ app/layout/footer.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+    <!-- Social Media Links -->
+    <div class="social-icons">
+        <a href="#"><img src="instagram-icon.png" alt="Instagram"></a>
+        <a href="#"><img src="x-icon.png" alt="X (Twitter)"></a>
+        <a href="#"><img src="linkedin-icon.png" alt="LinkedIn"></a>
+        <a href="#"><img src="facebook-icon.png" alt="Facebook"></a>
+    </div>
+
+    <!-- Copyright and Country Info -->
+    <div class="copyright">
+        &copy; <?php echo date("Y"); ?> MAKEMYTRIP PVT. LTD.
+    </div>
+    <div class="countries">
+        Country <span>India USA UAE</span>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ app/layout/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Copy of makemytrip.com</title>
+
+    <!-- Bootestrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <!-- Font Awesome 5 CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    
+    <!-- css files  -->
+    <link rel="stylesheet" href="./css/footer.css">
+    
+
+    <!-- js files  -->
+    <!-- <script defer src="./js/slider.js"></script> -->
+
+
+</head>
+<body>
