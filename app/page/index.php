ea8bff366f40a105b5aa8aec7b1dcc173084d619
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-18 14:37:48 +0530

    home

--- app/page/index.php
+++ app/page/index.php
@@ -1,9 +1,21 @@
 <!-- Include the header -->
 <?php include('../layout/header.php'); ?>
 
+<div class="bgGradient">
+
+</div>
 
 <div style="min-height:100vh"></div>
 
+<style>
+.bgGradient{
+background: #f2f2f2;
+background-repeat: no-repeat;
+background-image: url(../media/bg7.jpg), linear-gradient(to bottom, #051322, #15457c);
+min-height: 100vh;
+transition: background 0.1s linear;
+}
+</style>
 
 <!-- Include the footer -->
 <?php include('../layout/footer.php'); ?>
