d1dc49ea125834050c9314f4a53e2cf948258e38
RinaDhanaba <98539420+RinaDhanaba@users.noreply.github.com>
2025-02-22 15:48:42 +0530

    position-relative

--- app/layout/home-static-navigation.php
+++ app/layout/home-static-navigation.php
@@ -13,7 +13,7 @@
         ?>
             <li class="nav-item <?= $active_class ?>"><a href="<?= $data['link'] ?>"><i class="<?= $data['icon'] ?>"></i> <?= $item ?></a></li>
         <?php endforeach; ?>
-        <li class="more-dropdown hidden m-0">
+        <li class="more-dropdown hidden m-0 position-relative">
             <div class="dropdown">
                 <div class="dropdown-button">More ▼</div>
                 <div class="dropdown-content" id="moreMenu"></div>
